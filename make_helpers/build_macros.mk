a97ccaff21830eb5a265b6f9ed302601671701e0
Kun Qin <kuqin@microsoft.com>
2024-05-23 10:27:15 -0700

    more pritns

--- make_helpers/build_macros.mk
+++ make_helpers/build_macros.mk
@@ -301,8 +301,14 @@ $(eval DEP := $(patsubst %.o,%.d,$(OBJ)))
 $(eval LIB := $(call uppercase, $(notdir $(1))))
 
 $(OBJ): $(2) $(filter-out %.d,$(MAKEFILE_LIST)) | lib$(3)_dirs
-	$$(ECHO) "  C0      $$(CC)"
-	$$(ECHO) "  CC      $$<"
+	$$(ECHO) "  CC      $$(CC)"
+	$$(ECHO) "  C0      $(LIB)"
+	$$(ECHO) "  C1      $$($(LIB)_CFLAGS)"
+	$$(ECHO) "  C3      $$(CFLAGS)"
+	$$(ECHO) "  C2      $(TF_CFLAGS)"
+	$$(ECHO) "  C4      $(MAKE_DEP)"
+	$$(ECHO) "  C5      $$@"
+	$$(ECHO) "  C6      $$<"
 	$$(Q)$$(CC) $$($(LIB)_CFLAGS) $$(TF_CFLAGS) $$(CFLAGS) $(MAKE_DEP) -c $$< -o $$@
 
 -include $(DEP)
